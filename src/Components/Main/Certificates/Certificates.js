93f7876374a45bb67e4197c43d402d60d0d7f137
cristian musto <71329756+CristianMusto@users.noreply.github.com>
2023-10-18 12:08:46 +0200

    Certificates section done

--- src/Components/Main/Certificates/Certificates.js
+++ src/Components/Main/Certificates/Certificates.js
@@ -1,10 +1,35 @@
 import styleClass from "./Certificates.module.scss";
 import { Element } from 'react-scroll';
 import CertificatesCard from "../../UI/CertificatesCard/CertificatesCard";
+import { useState, useEffect } from "react";
 const Certificates = () => {
+    const [certificateContent, setCertificateContent] = useState([]);
+    const [baseUrl, setBaseUrl] = useState();
+    useEffect(() => {
+        setBaseUrl("https://cristianmusto.github.io/portfolio2.0/")
+        const knowledgeFetch = async () => {
+        const data = await (
+            await fetch(
+            "https://cristianmusto.github.io/portfolio2.0/json/cardsCertificate.json"
+            )
+        ).json();
+        setCertificateContent(data);
+        };
+        knowledgeFetch();
+    }, []);
+
     return (
-        <Element className={styleClass.CertificatesContainer} name="Certificates">
-            <CertificatesCard />
+        <Element className={styleClass.certificatesContainer} name="Certificates">
+            <div className={styleClass.certificatesContent}>
+            {certificateContent.length > 0 ? (
+              certificateContent.map((el) => (
+                <CertificatesCard className={`${styleClass.div}`} key={el.id} src={`${el.link}`} id={el.id} title={el.title} desc={el.description} 
+                shadow={el.shadow} image={`${baseUrl}${el.logo}`}/>
+              ))
+              ) : (
+                <h3>Coming Soon...</h3>
+            )}
+            </div>
         </Element>
     )
 } 
--- src/Components/UI/CertificatesCard/CertificatesCard.js
+++ src/Components/UI/CertificatesCard/CertificatesCard.js
@@ -3,8 +3,16 @@ import "./CertificatesCard.scss";
 const CertificatesCard = (props) => {
     
     return (
-        <div className="card">
-            
+        <div className={`${props.className} cardCertificate`} id={props.id} style={{backgroundImage: `url("${props.image}")`,  boxShadow: `0 0 20px 10px ${props.shadow}`}}>
+            <p className="heading">{props.title}</p>
+            <p className="para">{props.desc}
+            </p>
+            {props.title !== 'Coming Soon' ?
+            <div>
+                <div className="overlay" style={{backgroundColor: `${props.shadow}`}}></div>
+                <a className="card-btn" href={props.src !== "" ? `${props.src}` : "undefined"} target="_blank" rel="noreferrer">Certificate</a>
+            </div>
+            : ''}
         </div>
     )
 }
