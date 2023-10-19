469703c1c5eefe698e983e4a2d7cb0bb4ea8ef5b
cristian musto <71329756+CristianMusto@users.noreply.github.com>
2023-10-19 14:25:12 +0200

    pdf fix

--- src/Components/UI/KnowledgeCard/KnowledgeCard.js
+++ src/Components/UI/KnowledgeCard/KnowledgeCard.js
@@ -8,7 +8,7 @@ const KnowledgeCard = (props) => {
         setDelay(Math.random() * 2)
     }, [])
     return (
-        <div className={`${styleClass.oneDiv} ${props.className}`} style={{animationDelay: `${delay}s`, boxShadow: `0 0 20px 10px ${props.shadow}`}} onClick={props.onClick} id={props.id}>
+        <div className={`${styleClass.oneDiv} ${props.className}`} style={{animationDelay: `${delay}s`, boxShadow: `0 0 20px 10px ${props.shadow}`, background: `${props.shadow}`}} onClick={props.onClick} id={props.id}>
             <img src={props.src} alt="profile" className={styleClass.image}/>
         </div>
     )
